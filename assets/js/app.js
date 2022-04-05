9dde943389780c3b24f3fb6612a334c61bc49d38
ravi7483 <somwadvaibhav@gmail.com>
2022-04-05 20:30:11 +0530

    functionbasic

---
+++ assets/js/app.js
@@ -0,0 +1,272 @@
+console.log("app.js file");
+
+// Function :- Function are nothing but the relationship in between input and output
+
+// Arrgument :- When we pass actual value while calling the function that values are called as Arrgument
+
+
+// addition
+var x = 10;
+var y =20;
+var result = x + y ;
+console.log(result);
+
+var x = 50;
+var y = 26 ;
+var result1 = x - y;
+console.log(result1);
+
+var x = 45;
+var y = 50;
+var result2 = x * y ;
+console.log(result2);
+
+var x = 40;
+var y = 50 ;
+var result3 = x / y;
+console.log(result3);
+
+console.log("after function");
+
+// Addition
+
+function addition(x,y){
+	var result = x + y;
+	console.log(result);
+}
+addition(10, 20);
+
+function ThrernumAdd(x,y,z){
+	var result = x + y + z;
+	console.log(result);
+}
+ThrernumAdd(30, 40, 50);
+
+function AddFourNum(x, y, z, p){
+	var result = x + y + z + p;
+	console.log(result);
+}
+AddFourNum(100, 200, 300, 400);
+
+function AddFiveNum (a,b,c,d,e){
+	var result =a+b+c+d+e;
+	console.log(result);	
+}
+AddFiveNum(10,20,30,40,50);
+
+function AddSixNum(a,s,d,f,g,h){
+	result = a+s+d+f+g+h;
+	console.log(result);
+}
+AddSixNum(100, 200,300,400,500,600);
+
+function AddSevenNum(q,w,e,r,t,y,u){
+	result = q+w+e+r+t+y+u;
+	console.log(result);
+}
+AddSevenNum(12,22,35,46,57,68,79);
+
+function AddEigthNum(a,s,d,f,g,h,j,k){
+	result = a+s+d+f+g+h+j+k;
+	console.log(result);
+}
+AddEigthNum(19,28,37,46,55,64,73,28);
+
+function AddNineNum(x,y,z,a,b,c,p,q,r){
+	result = x+y+z+a+b+c+p+q+r;
+	console.log(result);	
+}
+AddNineNum(92,81,73,64,45,45,33,24,15);
+
+function AddTenNum(a,b,c,p,q,r,x,y,z,v){
+	result = a+b+c+p+q+r+x+y+z+v;
+	console.log(result);	
+}
+AddTenNum(101010,999,888,777,6666,555,444,333,222,111);
+
+// substract
+
+function SubNumbertwo(x,y){
+	var result = x - y;
+	console.log(result);	
+}
+SubNumbertwo(39,29);
+
+function SubThreeNum(a,b,c){
+	result = a-b-c;
+	console.log(result);
+}
+SubThreeNum(50,20,10);
+
+function SubFourNum(x, y, z, p){
+	result= x - y - z - p;
+	console.log(result);
+}
+SubFourNum(70,56,77,33);
+
+function SubFiveNum(a,s,d,f,g){
+	result = a-s-d-f-g;
+	console.log(result);
+}
+SubFiveNum(99,77,55,33,22);
+
+function SubSixNum(x,y,z,a,b,c){
+	result = -x -y -z -a -b -c;
+	console.log(result);	
+}
+SubSixNum(100,300,456,333,444,555);
+
+function SubSevenNum(a,b,c,x,y,z,o){
+	result=a - b - c - x - y - z - o;
+	console.log(result);
+}
+SubSevenNum(990,890,678,564,450,345,667,890,777);
+
+function SubEightNum(a,b,c,x,y,z,p,q){
+	result = a - b - c - x - y - z - p - q;
+	console.log(result);
+}
+SubEightNum(6666,6666,3333,666,777,888,9999,333);
+
+function SubNineNum(a,b,c,x,y,z,p,q,r){
+	result = a-b-c-x-y-z-p-q-r;
+	console.log(result);
+}
+SubNineNum(999,888,777,666,555,444,333,222,111);
+
+function SubTenNum(x,y,z,a,b,c,p,q,r,o){
+	result = x-y-z-a-b-c-p-q-o;
+	console.log(result);
+}
+SubTenNum(1000,999,888,777,666,555,444,333,222,111);
+
+// Multiplication
+
+function MulNumTwo(x,y){
+	var result = x * y;
+	console.log(result);
+}
+MulNumTwo(10, 20);
+
+function MulThreeNum(x,y,z){
+	var result = x * y * z;
+	console.log(result);
+}
+MulThreeNum(10,30,40);
+
+function MulFourNum(x,y,z,a){
+	var result = x*y*z*a;
+	console.log(result);
+}
+MulFourNum(50,40,40,40);
+
+function MulFiveNum(x,y,z,p,q){
+	var result = x*y*z*p*q;
+	console.log(result);
+}
+MulFiveNum(20,30,45,56,45);
+
+function MulSixNum(x,y,z,o,p,q){
+	var result = x * y * z * o * p * q;
+	console.log(result);
+}
+MulSixNum(30,40,30,40,50,40);
+
+function MulSevenNum(x,y,z,o,p,q,r){
+	var result = x * y * z * o * p * q  * r;
+	console.log(result);	
+}
+MulSevenNum(10,30,45,67,89,54,67);
+
+function MulEightNum(x,y,z,o,p,q,r,s){
+	var result = x * y * z * o * p * q * q * r * s;
+	console.log(result);
+}
+MulEightNum(12,34,56,67,78,89,90,45);
+
+function MulNineNum(x,y,z,o,p,q,r,s,t){
+	var result = x * y * z * o * p * q * r * s * t;
+	console.log(result);	
+}
+MulNineNum(12,34,56,78,89,34,34,56,78);
+
+function MulTenNum(x,y,z,o,p,q,r,s,t,u){
+	var result = x * y * z * o * p * q * r * s * t * u;
+	console.log(result);	
+}
+MulTenNum(12,34,56,78,90,23,45,34,56,78);
+
+
+// Division
+
+function DivNumTwo(x, y){
+	var result = x / y ;
+	console.log(result);
+}
+DivNumTwo(20, 30);
+
+function DivNumThree(x, y, z){
+	var result = x / y / z ;
+	console.log(result);
+}
+DivNumThree(20, 30, 50);
+
+function DivNumFour(x,y,z,p){
+	var result = x / y / z / p;
+	console.log(result);
+}
+DivNumFour(50,45,12,23);
+
+function DivFiveNum(x,y,z,p,q){
+	var result = x / y / z / p / q;
+	console.log(result);
+}
+DivFiveNum(13,45,67,89,56);
+
+function DivSixNum(x,y,z,p,q,r){
+	var result = x / y / z / p / q / r;
+	console.log(result);
+}
+DivSixNum(40,50,60,70,80,90);
+
+function DivSevenNum(x,y,z,p,q,r,s){
+	var result = x / y / z / p / q / r / s;
+	console.log(result);
+}
+DivSevenNum(24,56,45,12,34,56,67);
+
+function DivEightNum(x,y,z,p,q,r,s,t){
+	var result = x / y / z / p / q / r / s / t ;
+	console.log(result);
+}
+DivEightNum(56,78,90,34,56,78,56,78);
+
+function DivNineNum(x,y,z,p,q,r,a,b,c){
+	var result = x / y / z / p / q / r / a / b / c;
+	console.log(result);
+}
+DivNineNum(56,67,43,23,13,56,78,90,45);
+
+function DivTenNum(x,y,z,o,p,q,r,s,u,t){
+	var result = x / y / z / o / p / q / r / s / u / t;
+	console.log(result);
+}
+DivTenNum(56,67,89,23,56,78,90,12,45,67);
+
+// Function with return statement
+
+function AddTwoNum(x,y){
+	result = x + y;
+	console.log(result);
+}
+AddTwoNum(34,67);
+document.write(result);
+// alert(result);
+
+// Function Without return
+
+function PrintName(){
+	console.log("function without return :IamRk");
+}
+PrintName();
+documen
